2508695aad5c881d58706f13b8c949cc3ffdfa2d
NAKUL GNANASHEKAR <Nakul.GnanashekarA@mindtree.com>
2022-06-24 11:45:44 +0530

    Commiting UniversityApp

---
+++ UniversityApp/src/main/java/com/springboot/University/MessageRespond.java
@@ -0,0 +1,20 @@
+package com.springboot.University;
+
+public class MessageRespond {
+String message;
+
+public MessageRespond(String message) {
+	
+	this.message = message;
+}
+
+public String getMessage() {
+	return message;
+}
+
+public void setMessage(String message) {
+	this.message = message;
+}
+
+
+}
---
+++ UniversityApp/src/main/java/com/springboot/University/Unirepository.java
@@ -0,0 +1,6 @@
+package com.springboot.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Unirepository extends JpaRepository<University,Integer>{
+
+}
